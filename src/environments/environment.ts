bdf69705967bae5e9a9bf7bda7813e8bd30df499
P.imesh <69477813+imesh-bit@users.noreply.github.com>
2025-08-28 12:29:51 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
+export const API_URL = "http://localhost:8080/api";
+
 export const environment = {
-  production: true, 
-  baseUrl: 'http://4.224.105.63:8080/api',
+  production: true,
+  apiUrl: API_URL
 };
-
-
